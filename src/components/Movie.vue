6c9331fcba2e4448a9011b6e237350f4e18cff7c
JD154 <jesus.blancof96@gmail.com>
2019-10-20 11:07:02 -0400

    Remove jsonq dependency to filter data

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -9,42 +9,37 @@
     <div class="card-overlay">
       <div class="card-content is-flex">
         <div class="upper-content">
-          <p class="movie-date is-size-7 has-text-left has-text-weight-light">Released on {{jsonHelper('Released')}}</p>
-          <h1 class="movie-title is-size-3 has-text-weight-bold is-uppercase">{{jsonHelper('Title')}}</h1>
-          <p class="movie-genre is-size-6 has-text-weight-light">{{jsonHelper('Genre')}}</p>
-          <p class="movie-plot is-size-6 has-text-weight-normal">{{jsonHelper('Plot')}}</p>
-          <p class="movie-director is-size6 has-text-weight-normal">Directed by {{jsonHelper('Director')}}</p>
+          <p class="movie-date is-size-7 has-text-left has-text-weight-light">Released on {{movie.Released}}</p>
+          <h1 class="movie-title is-size-4 has-text-weight-bold is-uppercase">{{movie.Title}}</h1>
+          <p class="movie-genre is-size-6 has-text-weight-light">{{movie.Genre}}</p>
+          <p class="movie-plot is-size-6 has-text-weight-normal">{{movie.Plot}}</p>
+          <p class="movie-director is-size6 has-text-weight-normal">Directed by {{movie.Director}}</p>
         </div>
-        <p class="movie-rating is-size-6 has-text-centered"><span>{{jsonHelper('imdbRating')}}</span> score with <span>{{jsonHelper('imdbVotes')}}</span> votes</p>
+        <p class="movie-rating is-size-6 has-text-centered"><span>{{movie.imdbRating}}</span> score with <span>{{movie.imdbVotes}}</span> votes</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import jsonq from '../../node_modules/jsonq';
 
 export default {
   name: 'Movie',
   props: {
-    movie: Array,
+    movie: Object,
   },
   data() {
     return {
-      jsonQobj: jsonq(this.movie),
       placeholderImage: 'https://www.gardensbythebay.com.sg/etc/designs/gbb/clientlibs/images/common/not_found.jpg',
     };
   },
   methods: {
-    jsonHelper(name) {
-      const finder = this.jsonQobj.find(name).firstElm();
-      return finder;
-    },
+
   },
   computed: {
+    // Check if poster in movie is empty to replace with default placeholder
     replaceImage() {
-      let image = '';
-      image = this.jsonHelper('Poster') === 'N/A' ? this.placeholderImage : this.jsonHelper('Poster');
+      const image = this.movie.Poster === 'N/A' ? this.placeholderImage : this.movie.Poster;
       return image;
     },
   },
@@ -67,18 +62,18 @@ export default {
   }
 
   .card-overlay {
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      right: 0;
-      /* background-color: rgba($color: #14339d, $alpha: 0.9); */
-      background: #7474BF;
-      background: linear-gradient(to top, #348AC7, #7474BF);
-      overflow: hidden;
-      width: 100%;
-      height: 100%;
-      opacity: 0;
-      transition: opacity .3s cubic-bezier(0.4, 0.0, 1, 1);
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    /* background-color: rgba($color: #14339d, $alpha: 0.9); */
+    background: #7474BF;
+    background: linear-gradient(to top, #348AC7, #7474BF);
+    overflow: hidden;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    transition: opacity .3s cubic-bezier(0.4, 0.0, 1, 1);
   }
 
   .card:hover .card-overlay {
