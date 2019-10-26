c0b9823fbe8c63aa0b6169ba3062f1615169dde4
JD154 <jesus.blancof96@gmail.com>
2019-10-26 11:38:09 -0400

    Minor changes

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default () => axios.create({
-  /* baseURL: 'https://moviefinder-vue.herokuapp.com', */
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://moviefinder-vue.herokuapp.com',
+  /* baseURL: 'http://localhost:5000', */
 });
